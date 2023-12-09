016ad598465f3866eaff53b600226b36f75d800c
LENOVO <LENOVO@DESKTOP-BH2L40U>
2023-12-09 16:35:59 +0200

    add user form

--- login/mylogin/views.py
+++ login/mylogin/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django import forms
+
+
 # Create your views here.
 
+class GetForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "placeholder":"Username"
+    }))
+    username.label = ""
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder":"Password"
+    }))
 def index(request):
     return render(request,'mylogin/index.html')
 
@@ -22,14 +34,31 @@ def signin(request):
             login(request,user)
             return HttpResponseRedirect(reverse('mylogin:userpage'))
         else:
-            render(request,'mylogin/signup.html')
+            return HttpResponseRedirect(reverse('mylogin:signup'))
 
     return render(request,'mylogin/signin.html',{
         "error":"no authenticated user",
     })
 
 def signup(request):
-    return render(request,'mylogin/signup.html')
+    err = ''
+    if request.method == "GET":
+        form = GetForm(request.GET)
+        if form.is_valid():
+            usern = form.cleaned_data["username"]
+            passw = form.cleaned_data["password"]
+            if not User.objects.filter(username=usern).exists():
+                newuser = User.objects.create_user(usern,password=passw)
+                newuser.save()
+                return HttpResponseRedirect(reverse('mylogin:signin'))
+            else:
+                err = 'this user exist'
+                
+    return render(request,'mylogin/signup.html',{
+        'form':GetForm(),
+        'err':err
+        
+    })
 
 def logout_views(request):
     logout(request)
